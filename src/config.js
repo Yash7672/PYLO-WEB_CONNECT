260349835a6f703d494d7f7e3666687899a2de78
Yash7672 <ryashwanth173@gmail.com>
2026-09-24 22:40:48 +0530

    Use direct website APK download

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-// Single source of truth for the PYLO app download link.
 export const DOWNLOAD_URL =
   import.meta.env.VITE_PYLO_DOWNLOAD_URL ||
-  'https://pyloconnect.netlify.app/download';
+  '/app-release.apk';
 
 export function hasDownloadUrl() {
   return DOWNLOAD_URL.length > 0;
